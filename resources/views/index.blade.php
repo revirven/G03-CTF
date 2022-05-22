237858ba524881040bda76e033df2f4fcb0dac6c
revirven <huydnevermore@gmail.com>
2022-05-22 06:23:55 -0700

    Updated UI

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -43,7 +43,7 @@
 
                         @auth
                             <button class="btn btn-outline-danger btn-shadow px-3 my-2 ml-0 ml-sm-1 text-left typewriter" onclick="(function(){window.location.href='{{ route('home.instruct') }}'})();">
-                                <h4>Hack on</h4>
+                                <h4>Hack on!</h4>
                             </button>
                         @endauth
                     </div>
